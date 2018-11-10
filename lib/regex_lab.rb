539d28ab061ca3c6776a870d0f34933e1f02edcc
Ezenwa Onukwue <ezenwa.onukwue@salesloft.com>
2018-11-10 21:30:57 +0000

    automatically backed up by learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -1,7 +1,7 @@
 def starts_with_a_vowel?(word)
   if word.scan(/\A+[aeiou]/) == true
     return true
-  elsif word.scan(/\A+[bcdfghjklmnpqrstvwxyz]/) == false
+  elsif word.scan(/\A+[bcdfghjklmnpqrstvwxyz]/) == true
     return false
   end
 end
