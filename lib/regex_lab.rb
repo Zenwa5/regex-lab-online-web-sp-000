0a5d8552e0a72d215d9dbf900b95218b54c5428f
Ezenwa Onukwue <ezenwa.onukwue@salesloft.com>
2018-11-10 21:29:48 +0000

    automatically backed up by learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -1,5 +1,5 @@
 def starts_with_a_vowel?(word)
-  if word.match(/\A+[aeiou]/) == true
+  if word.scan(/\A+[aeiou]/) == true
     return true
   elsif word.scan(/\A+[bcdfghjklmnpqrstvwxyz]/) == false
     return false
