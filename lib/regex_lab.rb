6b791a2958ebb6d6cab171195ca7640cae19b4a6
Ezenwa Onukwue <ezenwa.onukwue@salesloft.com>
2018-11-10 22:46:44 +0000

    automatically backed up by learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -25,7 +25,7 @@ def first_word_capitalized_and_ends_with_punctuation?(text)
 end
 
 def valid_phone_number?(phone)
-if phone.match(/\d1234567890/)
+if phone.match(/\A\d[0-9]/)
   return true
 else
   return false
